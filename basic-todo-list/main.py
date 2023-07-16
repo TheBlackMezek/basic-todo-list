79c1311e7cd1cd7e4d07a4e5c4cc778466b8caee
TheBlackMezek <31017211+TheBlackMezek@users.noreply.github.com>
2023-07-15 20:09:43 -0700

    Make task names editable

--- basic-todo-list/main.py
+++ basic-todo-list/main.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, make_response
 import sql_handler
-from sql_handler import _DB_SESSION, Task, _BASE
+from sql_handler import _DB_SESSION, Task
 
 
 app = Flask(__name__)
 sql_handler.init()
 
-print(_BASE.metadata.tables.keys())
-results = Task.query.all()
-print(results)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -21,9 +18,24 @@ def index():
 	tasks = Task.query.all()
 	return render_template('base.html', tasks=tasks)
 
+
+@app.route('/set-task-name', methods=['POST'])
+def set_task_name():
+	req = request.get_json()
+	print(f"Setting name of task {req['task_id']} to \"{req['task_name']}\"")
+
+	task = _DB_SESSION.query(Task).filter_by(id=req['task_id']).first()
+	task.name = req['task_name']
+	_DB_SESSION.commit()
+
+	res = make_response(jsonify({"message": "Task name changed"}), 200)
+	return res
+
+
 @app.teardown_appcontext
 def shutdown_session(exception=None):
 	_DB_SESSION.remove()
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
